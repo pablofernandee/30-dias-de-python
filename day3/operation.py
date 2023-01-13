a2b458c71f51f52c97b56b0e72cc1b799c916d83
pablofernandee <pfernandezcolegio@gmail.com>
2023-01-13 11:10:32 +0100

    day3

---
+++ day3/operation.py
@@ -0,0 +1,55 @@
+age='16'
+print(int(age))
+ 
+height='176'
+print(float(height))
+
+cn1=2+3j
+print(cn1)
+cn2=2+4j
+print(cn2)
+cuenta_cn=cn1+cn2
+print(cuenta_cn)
+
+
+base = input(int("Enter base: "))
+print(base)
+altura= input(int("Enter altura: "))
+print(altura)
+base*altura
+cuenta_1=(base*altura)
+print(cuenta_1)
+formula=((base*altura)/2)
+area_triangle=(cuenta_1/2)
+print(area_triangle)
+
+
+
+
+
+
+side_a = input(int("Enter side a: "))
+print(side_a)
+side_b = input(int("Enter side b: "))
+print(side_b)
+side_c = input(int("Enter side c: "))
+print(side_c)
+perimetre_triangle=side_a+side_b+side_c
+print(perimetre_triangle)
+
+length = input('Enter length: ')
+width =input('Enter width: ')
+area_of_rectangle = length * width
+print('Area of rectangle:', area_of_rectangle)
+
+
+pi=3,14
+print(pi)
+radious=input(int('Enter radious: '))
+area_circule = pi*radious*radious
+print(area_circule)
+circunference = 2*pi*radious
+print(circunference)
+
+y=2
+x=-2
