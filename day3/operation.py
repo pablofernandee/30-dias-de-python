36a362162c7506deb26e77873cca0af5f719d285
pablofernandee <pfernandezcolegio@gmail.com>
2023-02-03 10:26:51 +0100

    day2

--- day3/operation.py
+++ day3/operation.py
@@ -37,15 +37,18 @@ print(side_c)
 perimetre_triangle=side_a+side_b+side_c
 print(perimetre_triangle)
 
-length = input('Enter length: ')
+length = input('Enter length: ') 
+print(length)
 width =input('Enter width: ')
+print(width)
 area_of_rectangle = length * width
 print('Area of rectangle:', area_of_rectangle)
 
 
 pi=3,14
 print(pi)
-radious=input(int('Enter radious: '))
+radious=input(int('Enter radious: ')) 
+print(radious)
 area_circule = pi*radious*radious
 print(area_circule)
 circunference = 2*pi*radious
@@ -53,3 +56,79 @@ print(circunference)
 
 y=2
 x=-2
+print(x)
+y=(2*(x-2))
+slope8=y
+print(slope8)
+
+x1=2
+print(x1)
+y1=2
+print(y1)
+x2=6
+print(x2)
+y2=10
+print(y2)
+m = y2-y1/x2-x1
+slope9=m
+print(slope9)
+print('the Euclidean distance is slope9')
+
+
+
+
+comparation=(slope8==slope9)
+print(comparation)
+
+
+variable_x=input('Enter a numer that do the variable y 0:')
+print(x)
+variable_y = variable_x^2 + 6*variable_x + 9
+print(variable_y)
+
+
+print('Python')
+lengthofpython=(len('Python'))
+print(lengthofpython)
+print('Dragon')
+lengthofdragon=(len('Dragon'))
+print(lengthofdragon)
+print('Dragon'=='aguacate')
+print('Python'=='futbol')
+
+
+print('on in Dragon and Python','on' in 'Dragon' and 'Python')
+
+
+print('jargon in I hope this course is not full of jragon','jargon' in 'I hope this course is not full of jargon' )
+
+length_python=(len('Python'))
+print(length_python)
+print(float(length_python))
+print(str(length_python))
+
+print('Even numbers are divisible by 2 and the remainder is zero')
+
+print((4%2) is 0)
+
+print((7//3)==(int (2.7)))
+
+print((type('10'))== (type(10)))
+
+print((int('9.8'))== 10)
+
+horas=input('Enter hours:')
+tarifa=input('Enter tarifa por hora:')
+print(horas*tarifa)
+
+
+
+
+
+
+
+
+
+
+
+
