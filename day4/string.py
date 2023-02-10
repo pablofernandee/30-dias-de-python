17763589de7655cc1d85b02caa9575ce9e9df392
pablofernandee <pfernandezcolegio@gmail.com>
2023-02-10 11:16:44 +0100

    day4

---
+++ day4/string.py
@@ -0,0 +1,61 @@
+string_1='Thirty'
+print(string_1)
+string_2='Days'
+print(string_2)
+string_3='Of'
+print(string_3)
+string_4='Python'
+print(string_4)
+formated_string=format(string_1,string_2,string_3,string_4)
+print(formated_string)
+
+string1_1='Coding'
+print(string1_1)
+string2_1='For'
+print(string2_1)
+string3_1='All'
+print(string3_1)
+formated_string2_1=str.format(string1_1,string2_1,string3_1)
+print(formated_string2_1)
+
+name_company='Coding for all'
+print(name_company)
+
+print(len(name_company))
+
+mayuscula=name_company.upper()
+print(mayuscula)
+minuscula=mayuscula.lower()
+print(minuscula)
+
+usarcapitalizado="Coding For All".capitalize()
+usartitle=usarcapitalizado.title()
+usarswapcase=usartitle.swapcase()
+
+cut='Coding for all'.split()
+for word in cut:
+ print(word[0])
+
+verificacion="Coding For All".index()
+print(verificacion)
+
+replace1="Coding For All".replace("Coding","Python")
+print(replace1)
+
+replace2=replace1.replace("All","Everyone")
+print(replace2)
+
+
+dividircoding="Coding For All".split()
+print(dividircoding)
+
+
+
+  
+
+
+
+
+
+
+
