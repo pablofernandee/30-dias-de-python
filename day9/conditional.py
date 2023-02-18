fcf1ea8e70c940d063ea8aae17c4bff4496dedbc
pablo <pfernandezcolegio@gmail.com>
2023-02-18 14:05:42 +0100

    day9

---
+++ day9/conditional.py
@@ -0,0 +1,51 @@
+age=input('Enter your age:')
+if age>= 18:
+ print('You are old enough to learn to drive')
+ if age<=18:
+  h=(18-age)
+  y='you need'
+  t='more years to learn to drive'
+print(y+h+t)
+
+myage=16
+yourage=input('Enter your age:')
+
+if myage==yourage:
+     print('you are the same age than me ')
+
+else:
+   print('you were born at the same age than me')
+
+if yourage>myage:
+     u='you are'
+     m=yourage-myage
+     ñ='older than me'
+     print(u+m+ñ) 
+else:
+   o=yourage-myage
+   print('you were born',o ,'before me')
+
+if yourage<myage:
+     q='you are'
+     l=myage-yourage
+     k='smaller than me'
+     print(q+l+k)
+else:
+   p=myage-yourage
+   print('you were born', p ,'after than me')
+
+
+
+numberone=input('Enter a number:')
+numbertwo=input('Enter a number:')
+if numberone>numbertwo:
+   print(numberone ,'is less than', numbertwo)
+else:
+   print(numberone ,'is greather than',numbertwo)
+
+if numberone<numbertwo:
+   print(numbertwo ,'is less than' ,numberone)
+else:
+   print(numbertwo ,'is greather than' ,numberone)
+
+   
