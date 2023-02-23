d081080ee2fa062f96b675f675e91efbe6dee090
pablo <pfernandezcolegio@gmail.com>
2023-02-23 20:17:53 +0100

    day9

--- day9/conditional.py
+++ day9/conditional.py
@@ -44,4 +44,81 @@ if numberone<numbertwo:
 else:
    print('{numbertwo} is greather than {numberone}')
 
+
+
+
+mark=input('Enter mark:')
+if mark==0<=49:
+   print(mark)
+else:
+   print('F')
+
+if mark>=50<=59:
+   print(mark)
+else:
+   print('D')
+
+if mark>=60<=69:
+   print(mark)
+else:
+   print('C')
+
+if mark>=70<=89:
+   print(mark)
+else:
+   print('B')
+
+if mark>=80<=100:
+   print(mark)
+else:
+   print('A')
+  
+
+
+season=input('Enter month:')
+
+if season=='September'or 'October' or 'November':
+   print(season)
+else:
+   print('The season is Autumn')
+
+if season=='December'or 'January' or 'February':
+   print(season)
+else:
+   print('The season is Winter')
+
+if season=='March'or 'April' or 'May':
+   print(season)
+else:
+   print('The season is Spring')
+
+if season=='June'or 'July' or 'August':
+   print(season)
+else:
+   print('The season is Summer')
+
+
+
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruta=input('Enter a fruit:')
+if fruta in fruits:
+   print(fruta)
+else:
+   print('That fruit already exist in the list')
+
+if fruta not in fruits:
+   print('fruta are not in the list')
+elif fruta not in fruits:
+   fruits.append(fruta)
+else:
+   print(fruits)
+
+
+
+
+
+
+
+
    
